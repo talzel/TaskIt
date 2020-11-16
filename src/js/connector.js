7688a71f6f9b142bbb320be16549fdf76ae5d5ce
talz <tal.zelig@planckresolution.com>
2020-11-16 17:44:34 +0200

    somechanges

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,4 +1,4 @@
-var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+var WHITE_ICON = '../img/house_hold_managment_icon.jpg';
 var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
 
 var onClickHouseManagementData = function (t, opts) {
