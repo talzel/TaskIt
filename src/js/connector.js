63a5d6c3c7c25658507b9e27e5764efb6d2287db
talz <tal.zelig@planckresolution.com>
2020-11-16 11:44:24 +0200

    added skeleton modal

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,45 @@ var BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4
 
 var onBtnClick = function (t, opts) {
   console.log('Someone clicked the button');
+  t.modal({
+    // the url to load for the iframe
+    url: './modal.html',
+    // optional arguments to be passed to the iframe as query parameters
+    // access later with t.arg('text')
+    args: { text: 'Hello' },
+    // optional color for header chrome
+    accentColor: '#F2D600',
+    // initial height for iframe
+    height: 500, // not used if fullscreen is true
+    // whether the modal should stretch to take up the whole screen
+    fullscreen: true,
+    // optional function to be called if user closes modal (via `X` or escape, etc)
+    callback: () => console.log('Goodbye.'),
+    // optional title for header chrome
+    title: 'appear.in meeting',
+    // optional action buttons for header chrome
+    // max 3, up to 1 on right side
+    actions: [{
+      icon: './images/icon.svg',
+      url: 'https://google.com',
+      alt: 'Leftmost',
+      position: 'left',
+    }, {
+      icon: './images/icon.svg',
+      callback: (tr) => tr.popup({
+        title: tr.localizeKey('appear_in_settings'),
+        url: 'settings.html',
+        height: 164,
+      }),
+      alt: 'Second from left',
+      position: 'left',
+    }, {
+      icon: './images/icon.svg',
+      callback: () => console.log(':tada:'),
+      alt: 'Right side',
+      position: 'right',
+    }],
+  })
 };
 
 window.TrelloPowerUp.initialize({
