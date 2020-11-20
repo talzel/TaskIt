f0e6e80ef45599b015e0028a44c7b353be31cd30
talz <tal.zelig@planckresolution.com>
2020-11-20 17:55:09 +0200

    bla6

--- src/js/household_management_data.js
+++ src/js/household_management_data.js
@@ -56,8 +56,40 @@ distincts = members.then(function(ms){
   return acc
 });
 
-console.log(distincts)
+console.log(distincts.values())
 console.log('-----------------------------------')
+
+data = distinct.values()
+
+var svg = d3.select("#weekly_chores"),
+width = svg.attr("width"),
+height = svg.attr("height"),
+radius = Math.min(width, height) / 2,
+g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var color = d3.scaleOrdinal(['#FFDDD3','#CBE1EF']);
+
+// Generate the pie
+var pie = d3.pie();
+
+// Generate the arcs
+var arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+//Generate groups
+var arcs = g.selectAll("arc")
+        .data(pie(data))
+        .enter()
+        .append("g")
+        .attr("class", "arc")
+
+//Draw arc paths
+arcs.append("path")
+.attr("fill", function(d, i) {
+    return color(i);
+})
+.attr("d", arc);
 
 
 // var cards = t.arg('cards')
